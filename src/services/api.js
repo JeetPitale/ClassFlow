78a4679eb40b1dab523bf8ff253c8a583458272f
SketchingCode <jeetdipakpitale1107@gmail.com>
2025-12-31 23:07:34 +0530

    feat: Add Azure App Service deployment configuration and update API endpoint.

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://classflow-backend-jeet.azurewebsites.net/api';
 
 const api = axios.create({
     baseURL: API_URL,
