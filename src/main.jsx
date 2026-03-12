15041a68182e839071569ee93acb87e7314ef653
SketchingCode <jeetdipakpitale1107@gmail.com>
2026-03-12 22:03:15 +0530

    disable F12

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from './App.jsx';
 import "./index.css";
 
+// Disable right-click across the entire application
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 createRoot(document.getElementById("root")).render(<App />);
