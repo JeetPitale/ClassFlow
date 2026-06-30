63db961eb7017cda368c257d203cb55fd8b5ad33
SketchingCode <jeetdipakpitale1107@gmail.com>
2026-06-30 20:31:11 +0530

    feat: disable right-click globally on the website

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from './App.jsx';
 import "./index.css";
 
-// Allow right-click across the entire application for debugging
-// document.addEventListener('contextmenu', event => event.preventDefault());
+// Disable right-click across the entire application
+document.addEventListener('contextmenu', event => event.preventDefault());
 
 createRoot(document.getElementById("root")).render(<App />);
